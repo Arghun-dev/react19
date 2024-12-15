7bafcd0caea2fd0f878b69b73131f52d3f84e80f
Arghava Mousanezhad Zangalani <amousane@volvocars.com>
2024-12-15 17:53:59 +0100

    useOptimistic

---
+++ src/actions/Optimistic.tsx
@@ -0,0 +1,28 @@
+import { useOptimistic } from "react";
+
+// Another common UI pattern when performing a data mutation is to show the final state optimistically while the async request is underway. In React 19, we’re adding a new hook called useOptimistic to make this easier:
+
+function ChangeName({ currentName, onUpdateName }) {
+  const [optimisticName, setOptimisticName] = useOptimistic(currentName);
+
+  const submitAction = async (formData) => {
+    const newName = formData.get("name");
+    setOptimisticName(newName);
+    const updatedName = await updateName(newName);
+    onUpdateName(updatedName);
+  };
+
+  return (
+    <form action={submitAction}>
+      <p>Your name is: {optimisticName}</p>
+      <p>
+        <label>Change Name:</label>
+        <input
+          type="text"
+          name="name"
+          disabled={currentName !== optimisticName}
+        />
+      </p>
+    </form>
+  );
+}
